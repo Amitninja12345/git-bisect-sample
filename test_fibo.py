77970aee27e848dc36e1407f62c6edc423d4865d
Simon Dollé <simon.dolle@gmail.com>
2014-03-27 22:45:49 +0100

    initial files

---
+++ test_fibo.py
@@ -0,0 +1,15 @@
+import unittest
+
+from fibo import fibo
+
+class FiboTestCase(unittest.TestCase):
+    def test_edge_case(self):
+        self.assertEqual(0, fibo(0))
+        self.assertEqual(1, fibo(1))
+
+    def test_nominal_case(self):
+        self.assertEqual(8, fibo(6))
+        self.assertEqual(55, fibo(10))
+
+if __name__ == "__main__":
+    unittest.main()
