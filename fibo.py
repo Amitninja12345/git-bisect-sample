4b756329f6ce61a0067590c0165e7909ea4a4089
Simon Dollé <simon.dolle@gmail.com>
2014-03-27 23:12:52 +0100

    handle negative values

--- fibo.py
+++ fibo.py
@@ -1,10 +1,14 @@
 def fibo(n):
     """Compute the nth Fibonacci number.
+    Raises if n is negative
     :param n: the index of the Fibonacci number we want to compute
     :type n: int
     :returns: int
+    :raises: ValueError
     """
-    if n == 0:
+    if n < 0:
+        raise ValueError("n must be positive.")
+    elif n == 0:
         return 0
     elif n == 1:
         return 1
--- test_fibo.py
+++ test_fibo.py
@@ -7,6 +7,11 @@ class FiboTestCase(unittest.TestCase):
         self.assertEqual(0, fibo(0))
         self.assertEqual(1, fibo(1))
 
+    def test_negative_param(self):
+        self.assertRaises(ValueError,
+                          fibo,
+                          -1)
+
     def test_nominal_case(self):
         self.assertEqual(8, fibo(6))
         self.assertEqual(55, fibo(10))
